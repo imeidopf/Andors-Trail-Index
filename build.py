c80eeee906196eb31439fbb30dcb816b7d50515b
Jason Imeidopf Jr <jsijr@live.com>
2020-05-09 18:36:07 -0400

    Questlist Updates
    Questlist files' json now read and printed to console.

--- build.py
+++ build.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 from modules import questlist, monsterlist, itemlist, itemcategories
 
+# Update this to reflect the version of Andor's Trail we want to use.
+# Place the xml and raw files inside the res folder. E.g. res/0.7.10
 appVersion = '0.7.10'
 
+# Run each module and build out the database material.
 questlist.build(appVersion)
-monsterlist.build(appVersion)
-itemlist.build(appVersion)
-itemcategories.build(appVersion)
+#monsterlist.build(appVersion)
+#itemlist.build(appVersion)
+#itemcategories.build(appVersion)
+
+# Done!
+#print("All resources have been built!")
--- modules/questlist.py
+++ modules/questlist.py
@@ -1,10 +1,24 @@
-import os
-import glob
+import os, glob, json
 
 def build(appVersion):
     count = 0
     path = "res/{}/raw/".format(appVersion)
+
     for file in glob.glob(path + "questlist*.json"):
+        filename = os.path.basename(file)
+        if filename != 'questlist_nondisplayed.json' and filename != 'questlist_debug.json':
+            f = open(file)
+            data = json.load(f)
+            for quest in data:
+                print("File: " + filename + "\r\n     " + quest['name'])
+                for stage in quest['stages']:
+                    if 'logText' in stage:
+                        print("          " + str(stage['progress']) + "% | " + stage['logText'])
+                    else:
+                        print("          " + str(stage['progress']) + "%")
+        else:
+            print('Skipped File: ' + filename)
+            continue
         count += 1
 
     print("Questlist Files Found: " + str(count))
