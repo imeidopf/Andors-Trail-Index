8e905da5dd3d65e54cd3002ba77ef63cd2fee61f
Jason Imeidopf <jsijr@live.com>
2021-06-05 23:16:23 -0400

    Map Generation
    Map images can be generated now!

--- modules/worldmap.py
+++ modules/worldmap.py
@@ -5,6 +5,9 @@ def generateMapImages(resPath, globFilter):
     print('Map files found: ' + str(len(globPath)))
     print('Generating map resources...')
 
+    totalFiles = len(globPath)
+    count = 0
+
     for file in globPath:
         # Parse the file and set width and height attributes. The script will fail if these are missing.
         tree = ET.parse(file)
@@ -26,7 +29,7 @@ def generateMapImages(resPath, globFilter):
         finalImage = pygame.Surface(levelSize)
 
         for layer in tiled_map.layers:
-            if layer.name == 'Walkable':
+            if layer.name != 'Ground' and layer.name != 'Objects' and layer.name != 'Above':
                 continue
             if isinstance(layer, pytmx.TiledTileLayer):
                 for x, y, gid in layer:
@@ -36,6 +39,9 @@ def generateMapImages(resPath, globFilter):
                         finalImage.blit(spritesheet, (x * tiled_map.tilewidth, y * tiled_map.tileheight), tileData[1])
 
         filename = os.path.basename(file)
-        pygame.image.save(finalImage, filename.split('.')[0])
+        pygame.image.save(finalImage, resPath + filename.split('.')[0] + '.png')
+
+        count = count + 1
+        print('Generated map ' + str(count) + ' of ' + str(totalFiles) + '...')
     
     print('Finished map image generation...')
